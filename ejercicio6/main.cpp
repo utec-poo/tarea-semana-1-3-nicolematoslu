3f82adb92ea01500904468426fc26f77e01bd57a
nicolematoslu <55298331+nicolematoslu@users.noreply.github.com>
2019-09-18 05:13:17 -0500

    Update main.cpp

--- ejercicio6/main.cpp
+++ ejercicio6/main.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 #include "Tipos.h"
 using namespace std;
-
 int main() {
-
-entero numero;
-
-
-cout << "ingrese un numero:";
-
+int numero,residuo;
+do{
+cout << "ingresar un numero:";
 cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
-  return 0;
-}
+} while ((numero<100) or (numero>1000)); 
+for (int i=0; residuo!= 0; i++)
+ {
+  residuo= numero/1;
+   if (residuo%2==0)
+   {cout<< " ultima cifra es par \n";}
+   else 
+    {cout<< " ultima cifra es impar \n";}
+  residuo= numero/10;
+  if (residuo%2==0)
+   {cout<< "segunda cifra es par \n";}
+   else 
+    {cout<< "segunda cifra es impar \n";}
+  residuo= numero/100;
+  if (residuo%2==0)
+   {cout<< " primera cifra es par \n";}
+   else 
+    {cout<< "primera cifra es impar \n";}
+  residuo = numero/1000;  
+ }
+} 
